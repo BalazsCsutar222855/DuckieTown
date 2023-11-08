638e6c523f9b6528433e661dfbcce50727548e2f
BalazsCsutar222855 <222855@buas.nl>
2023-11-08 13:17:02 +0100

    Duckietown instructions

--- Environment/test.py
+++ Environment/test.py
@@ -1,7 +1,33 @@
-# First import the gym
 import gym
-env = gym.make('CartPole-v1')
+import gym_duckietown
+from IPython.display import display, clear_output
+from PIL import Image
+
+# Create the environment
+env = gym.make('Duckietown-loop_obstacles-v0')
+env.reset()
 
 
-action = env.action_space.sample() # take a random action
-print(action)
+# Helper function to display an image in Jupyter notebook
+def show_array(arr):
+    img = Image.fromarray(arr, 'RGB')
+    # Make the image smaller to increase the FPS of the notebook
+    img = img.resize((320, 240))
+    display(img)
+
+
+# Run the simulation until we're done
+done = False
+while not done:
+    action = env.action_space.sample()
+    observation, reward, done, info = env.step(action)
+    frame = env.render(mode='rgb_array')
+
+    # Clear the previous output and display the new frame
+    clear_output(wait=True)
+    show_array(frame)
+
+    if done:
+        env.reset()
+
+env.close()
